31517769d88420ad2e06efdea1462d7671e719cc
one10 <ivan.balepin@gmail.com>
2017-12-19 05:51:28 -0800

    unit test fix, added repo for a dep module

--- org.one10.examples.two/src/test/java/org/one10/examples/two/BackendServiceTwoTest.java
+++ org.one10.examples.two/src/test/java/org/one10/examples/two/BackendServiceTwoTest.java
@@ -5,19 +5,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class MainTest {
+public class BackendServiceTwoTest {
     @Test
-    public void testRunModuleUtil() {
-        assertEquals("module util", "token1", Main.runModuleUtil());
+    public void testBackendServiceTwo() {
+        assertEquals("BackendServiceTwo test", "BackendServiceTwo: token1", new BackendServiceTwo().getBackendServiceName());
     }
 
-    @Test
-    public void testRunModuleOne() {
-        assertEquals("module 1", "BackendServiceOne", Main.runModuleOne());
-    }
-
-    @Test
-    public void testRunModuleTwo() {
-        assertEquals("module 2", "BackendServiceTwo: token1", Main.runModuleTwo());
-    }
 }
