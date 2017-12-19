36685a541c4e9f364c6667022944413463fa9ac4
one10 <ivan.balepin@gmail.com>
2017-12-19 05:46:03 -0800

    added unit tests

--- src/main/java/org/one10/examples/mainapp/Main.java
+++ src/main/java/org/one10/examples/mainapp/Main.java
@@ -7,10 +7,22 @@ import org.one10.examples.util.TokenUtil;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.format("Java 9 module org.one10.examples.util: %s%n", new TokenUtil().getToken());
-        System.out.format("Java 9 module org.one10.examples.one: %s%n", new BackendServiceOne().getBackendServiceName());
-        System.out.format("Java 9 module org.one10.examples.two: %s%n", new BackendServiceTwo().getBackendServiceName());
+        System.out.format("Java 9 module org.one10.examples.util: %s%n", runModuleUtil());
+        System.out.format("Java 9 module org.one10.examples.one: %s%n", runModuleOne());
+        System.out.format("Java 9 module org.one10.examples.two: %s%n", runModuleTwo());
 
         System.out.println("*** Done");
+    }
+
+    public static String runModuleUtil() {
+        return new TokenUtil().getToken();
+    }
+
+    public static String runModuleOne() {
+        return new BackendServiceOne().getBackendServiceName();
+    }
+
+    public static String runModuleTwo() {
+        return new BackendServiceTwo().getBackendServiceName();
     }
 }
---
+++ src/test/java/org/one10/examples/mainapp/MainTest.java
@@ -0,0 +1,23 @@
+package org.one10.examples.mainapp;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testRunModuleUtil() {
+        assertEquals("module util", "token1", Main.runModuleUtil());
+    }
+
+    @Test
+    public void testRunModuleOne() {
+        assertEquals("module 1", "BackendServiceOne", Main.runModuleOne());
+    }
+
+    @Test
+    public void testRunModuleTwo() {
+        assertEquals("module 2", "BackendServiceTwo: token1", Main.runModuleTwo());
+    }
+}
