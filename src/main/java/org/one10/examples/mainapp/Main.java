e2e2fbac1770614f15d91d229f3e794c1953b55c
one10 <ivan.balepin@gmail.com>
2017-12-19 04:53:32 -0800

    added IntelliJ module dependencies

--- src/main/java/org/one10/examples/mainapp/Main.java
+++ src/main/java/org/one10/examples/mainapp/Main.java
@@ -1,8 +1,10 @@
 package org.one10.examples.mainapp;
 
+import org.one10.examples.util.TokenUtil;
+
 public class Main {
     public static void main(String[] args) {
-//        TokenUtil t1 = new TokenUtil();
+        TokenUtil t1 = new TokenUtil();
 
         System.out.println("*** Done");
     }
