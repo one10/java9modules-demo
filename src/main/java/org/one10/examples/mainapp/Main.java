14905ed604af6b4666f6d80d69f271ec9e5ccb48
one10 <ivan.balepin@gmail.com>
2017-12-19 03:27:47 -0800

    adding submodules

--- src/main/java/org/one10/examples/mainapp/Main.java
+++ src/main/java/org/one10/examples/mainapp/Main.java
@@ -2,6 +2,8 @@ package org.one10.examples.mainapp;
 
 public class Main {
     public static void main(String[] args) {
+        TokenUtil t1 = new TokenUtil();
+
         System.out.println("*** Done");
     }
 }
---
+++ src/org.one10.examples.util/module-info.java
@@ -0,0 +1,1 @@
+module org.one10.examples.util {}
