2aaf4f8042e557a59f8d7526d533aaaad206d193
one10 <ivan.balepin@gmail.com>
2017-12-19 03:32:56 -0800

    defining sub-modules in top-level build.gradle

--- src/main/java/org/one10/examples/mainapp/Main.java
+++ src/main/java/org/one10/examples/mainapp/Main.java
@@ -2,7 +2,7 @@ package org.one10.examples.mainapp;
 
 public class Main {
     public static void main(String[] args) {
-        TokenUtil t1 = new TokenUtil();
+//        TokenUtil t1 = new TokenUtil();
 
         System.out.println("*** Done");
     }
